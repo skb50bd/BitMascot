e0998133b969dbbccc1fc7688c488aa0c8d666ce
Shakib Haris <skb50bd@users.noreply.github.com>
2020-01-30 13:28:12 +0600

    Fixed Skips in Printing

--- src/RSSFetch/JpegFinder.cs
+++ src/RSSFetch/JpegFinder.cs
@@ -34,12 +34,12 @@ namespace BitMascot
             var time = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
             var path = Configuration.LocalFilePath;
             var count = urls?.Count() ?? 0;
-            var message = $"Found {count } JPEG images in {path} [{time}]";
+            var message = $"Found {count} JPEG images in {path} [{time}]";
             Console.WriteLine(message);
 
             for(var i = 0; i < urls.Count(); i++) {
                 var url = urls[i];
-                Console.WriteLine($"{++i} - {url}");
+                Console.WriteLine($"{i + 1} - {url}");
             }
             Console.WriteLine("\n");
         }
